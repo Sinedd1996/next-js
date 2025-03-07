24809aa548980ad2c74d52194459c32fea8fd7b5
i.gul <dell23qqq@mail.ru>
2025-03-07 16:05:49 +0300

    feat: auth context 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isAuth, setIsAuth] = useState<boolean | undefined>(false);
+  const [isAuth, setIsAuth] = useState<boolean | undefined>(pageProps.isAuth || false);
 
   return (
     <AuthContext.Provider value={{ isAuth, setIsAuth }}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import AuthContext from "@/context/authContext";
-import { useContext } from "react";
-
 export default function Home() {
-  const { setIsAuth } = useContext(AuthContext);
-
-  setIsAuth(true)
   return <div>Main PAGE</div>;
 }
+
+export const getServerSideProps = async () => {
+  const isAuth = true;
+
+  return { props: { isAuth } };
+};
