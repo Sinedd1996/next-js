d6a5865831c1fbf2b24a5e0a712ef21d8d7ae178
Sinedd23 <dell23qqq@mail.ru>
2025-03-20 00:50:26 +0300

    style: адаптивные стили

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -34,9 +34,9 @@ export function Modal({ children, onClose, isOpen }: ModalProps) {
         className="fixed inset-0 bg-gray-500/75 transition-opacity"
         aria-hidden="true"
       />
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative py-[48px] px-[40px] overflow-hidden rounded-lg bg-white text-left w-[500px] max-w-full">
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-white tablet:bg-transparent">
+        <div className="flex min-h-full justify-center p-4 tablet:items-center">
+          <div className="relative pt-[56px] tablet:py-[48px] tablet:px-[40px] overflow-hidden rounded-lg bg-white text-left w-[500px] max-w-full">
             <button
               onClick={onClose}
               className="absolute right-3 top-3 hover:text-blue-500 bg-gray-500 rounded-full p-1 hover:bg-black"
